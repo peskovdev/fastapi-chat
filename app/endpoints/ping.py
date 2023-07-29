82426815dc6e95fde691a782be016516c079bcf2
Peskov Sergey <peskovdev@proton.me>
2023-07-30 01:06:51 +0600

    Set up app without db

---
+++ app/endpoints/ping.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from starlette import status
+
+from app.schemas import PingResponse
+
+
+router = APIRouter(
+    prefix="/health_check",
+    tags=["Application Health"],
+)
+
+
+@router.get(
+    "/ping_application",
+    response_model=PingResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def ping_application() -> dict:
+    return {"message": "Application worked!"}
