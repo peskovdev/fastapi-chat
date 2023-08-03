b5988160f9d815bd1d3753ec096bac3225041071
Peskov Sergey <peskovdev@proton.me>
2023-08-03 19:43:19 +0600

    temp

--- app/db/connection/session.py
+++ app/db/connection/session.py
@@ -1,7 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+# TODO: remove next line
+# from sqlalchemy.orm import sessionmaker
 
-from bookmarker.config import get_settings
+from app.config import settings
 
 
 class SessionManager:
@@ -18,11 +19,13 @@ class SessionManager:
             cls.instance = super(SessionManager, cls).__new__(cls)
         return cls.instance  # noqa
 
-    def get_session_maker(self) -> sessionmaker:
-        return sessionmaker(self.engine, class_=AsyncSession, expire_on_commit=False)
+    def get_session_maker(self) -> async_sessionmaker:
+        return async_sessionmaker(self.engine, expire_on_commit=False)
+        # TODO: remove next line
+        # return async_sessionmaker(self.engine, class_=AsyncSession, expire_on_commit=False)
 
     def refresh(self) -> None:
-        self.engine = create_async_engine(get_settings().database_uri, echo=True, future=True)
+        self.engine = create_async_engine(settings.database_uri, echo=True, future=True)
 
 
 async def get_session() -> AsyncSession:
